7e3c5aa053dfd1997880990446f728bce527890b
Raul Jimenez <jcr0034569@est.univalle.edu>
2026-09-15 17:07:13 -0400

    Avance papas

--- Assets/BurguerKing/01_Scripts/04_Trash/TrashBin.cs
+++ Assets/BurguerKing/01_Scripts/04_Trash/TrashBin.cs
@@ -2,12 +2,25 @@ using UnityEngine;
 
 public class TrashBin : MonoBehaviour
 {
+    [Header("REFERENCIA")]
+    public BurgerAssembly burgerAssembly;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Ingredient"))
+        if (other.CompareTag("Ingredient") ||
+            other.CompareTag("Burger") ||
+            other.GetComponent<FriesBag>() != null ||
+            other.GetComponent<DrinkCup>() != null)
         {
 
+            bool isBurger = other.CompareTag("Burger");
+
             Destroy(other.gameObject);
+
+            if (isBurger && burgerAssembly != null)
+            {
+                burgerAssembly.ResetAssembly();
+            }
         }
     }
 }
